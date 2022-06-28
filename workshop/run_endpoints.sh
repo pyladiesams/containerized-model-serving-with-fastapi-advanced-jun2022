779c99cc3852c45bf7fd6e7d3ba46c92b7b5064b
nvanommeren <nikki_vanommeren@hotmail.com>
2022-06-28 19:34:38 +0200

    add solutions

--- workshop/run_endpoints.sh
+++ workshop/run_endpoints.sh
@@ -1,2 +1,7 @@
-docker build
-docker run -i -t --name music_style bash -c "python src/spotify.py"
+# docker build -t music_style_image .
+# docker run --env-file=.env -i -t -p 8080:8080 --name music_style -v "/${PWD}"/src:/code/src music_style_image bash -c "python src/spotify.py"
+
+docker build -t music_style_image .
+docker run --env-file=.env -i -t -p 8080:8080 --name music_style -v "/${PWD}"/src:/code/src music_style_image bash -c "uvicorn src.main:app --host 0.0.0.0 --port 8080 --reload"
+# docker tag music_style_image:latest karlijnschipper/pyladies_fastapi:latest
+# docker push karlijnschipper/pyladies_fastapi:latest
